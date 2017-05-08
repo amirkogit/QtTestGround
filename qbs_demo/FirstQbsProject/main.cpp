d1b9093f6483523dbc039d792cd4be88dd4f4dbe
amirkogit <amirkomail@gmail.com>
2017-05-08 14:41:53 -0400

    misssed the main file in the previous commit

---
+++ qbs_demo/FirstQbsProject/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    cout << "This is the first application that uses QBS build system.\n";
+
+    return 0;
+}
