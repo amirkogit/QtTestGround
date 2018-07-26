7b77f75351b652a52015d4619fd705caa29e9a04
Amir Shrestha <amirkomail@gmail.com>
2018-07-26 10:19:48 -0400

    minor formatting and uploaded some test DICOM files available in pydicom

--- pydicom_pyside2/parse_dicom.py
+++ pydicom_pyside2/parse_dicom.py
@@ -98,12 +98,10 @@ class Window(QtWidgets.QDialog):
     def createFilesTable(self):
         self.filesTable = QtWidgets.QTableWidget(0, 2)
         self.filesTable.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
-
         self.filesTable.setHorizontalHeaderLabels(("File Name", "Size"))
         self.filesTable.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         self.filesTable.verticalHeader().hide()
         self.filesTable.setShowGrid(False)
-
         self.filesTable.cellActivated.connect(self.openFileOfItem)
 
     def displayDicomInformation(self, msg):
